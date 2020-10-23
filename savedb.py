e1450c316e4abc790fcd666d9f2ac7f4f6db1364
if <if>
2020-10-23 22:37:04 +0300

    hotfix

--- savedb.py
+++ savedb.py
@@ -16,37 +16,49 @@ def connect_to_db(db_url=DATABASE_URL):
 
 def truncate(table, conn):
     """ truncate temp table """
-    with conn:
-        curs = conn.cursor()
-        curs.execute(f"TRUNCATE TABLE {table}_temp")
+    try:
+        with conn:
+            curs = conn.cursor()
+            curs.execute(f"TRUNCATE TABLE {table}_temp")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return False
 
 
 def save_to_temp_db(image, table, conn, image_name='img'):
     """ insert a BLOB into a temp table """
-    with conn:
-        curs = conn.cursor()
-        curs.execute(f"""
-            INSERT INTO {table}_temp (orig_filename, file_data)
-            VALUES ('img', {psycopg2.Binary(image)})
-        """)
-        conn.commit()
+    try:
+        with conn:
+            curs = conn.cursor()
+            curs.execute(f"""
+                INSERT INTO {table}_temp (orig_filename, file_data)
+                VALUES ('img', {psycopg2.Binary(image)})
+            """)
+            conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return False
 
 
 def update_prod_db(table, conn):
     """ update prod table """
-    with conn:
-        curs = conn.cursor()
-        curs.execute(f"""
-            INSERT INTO {table}(orig_filename, file_data)
-                SELECT DISTINCT orig_filename, file_data
-                FROM {table}_temp temp
-                WHERE NOT EXISTS (
-                        SELECT 1
-                        FROM {table} prod
-                        WHERE temp.file_data = prod.file_data
-                )
-        """)
-        conn.commit()
+    try:
+        with conn:
+            curs = conn.cursor()
+            curs.execute(f"""
+                INSERT INTO {table}(orig_filename, file_data)
+                    SELECT DISTINCT orig_filename, file_data
+                    FROM {table}_temp temp
+                    WHERE NOT EXISTS (
+                            SELECT 1
+                            FROM {table} prod
+                            WHERE temp.file_data = prod.file_data
+                    )
+            """)
+            conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return False
 
 
 def save_img(data, filename):
@@ -56,18 +68,21 @@ def save_img(data, filename):
 
 def load(table, path_to_dir, img_id, conn, ext=".png"):
     """ read BLOB data from a table """
+    try:
+        with conn:
+            curs = conn.cursor()
+            curs.execute(f"""
+                SELECT orig_filename, file_data
+                FROM {table}
+                WHERE id = {img_id}
+            """)
 
-    with conn:
-        curs = conn.cursor()
-        curs.execute(f"""
-            SELECT orig_filename, file_data
-            FROM {table}
-            WHERE id = {img_id}
-        """)
-
-        blob = curs.fetchone()
-        print(path_to_dir + ext)
-        save_img(blob[1], path_to_dir + blob[0] + ext)
+            blob = curs.fetchone()
+            print(path_to_dir + ext)
+            save_img(blob[1], path_to_dir + blob[0] + ext)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return False
 
 
 def main():
