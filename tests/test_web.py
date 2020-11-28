63dc3c6af5e52ad8751c7a46a38a1fb975626c19
if <if>
2020-11-28 08:20:13 +0300

    add virtual display

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,18 +1,22 @@
 import os
 import pytest
 import pyautogui
+import Xlib.display
 from time import sleep
 from selenium import webdriver
+from pyvirtualdisplay import Display
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+display = Display(visible=1, size=(1600, 900))
+
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
-zebrate_url = "https://zebrate.herokuapp.com/"
-# zebrate_url = "http://localhost:8501/"
+# zebrate_url = "https://zebrate.herokuapp.com/"
+zebrate_url = "http://localhost:8501/"
 horse_url = "https://media.istockphoto.com/photos/wild-horses-running-free-picture-id1019461046?k=6&m=1019461046&s=612x612&w=0&h=RN04ILkViwine-g3B9BjtUmGV_mucSiQgv8mKM-F644="
 
 
@@ -28,7 +32,7 @@ def test_put_image_link():
     driver.get(zebrate_url)
     sleep(5)
     link_inputer = driver.find_element(By.TAG_NAME, 'input')
-    link_inputer.send_keys(horse_url)    # заполнить
+    link_inputer.send_keys(horse_url)  # заполнить
     link_inputer.send_keys(Keys.RETURN)  # отправить
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     sleep(5)
@@ -36,16 +40,21 @@ def test_put_image_link():
 
 
 def test_send_image_file():
-    os.environ['DISPLAY'] = ':0'
+    display.start()
+    pyautogui._pyautogui_x11._display = Xlib.display.Display(
+        os.environ['DISPLAY']
+    )
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    driver.set_window_size(1600, 900)
     driver.get(zebrate_url)
     sleep(5)
     btn = driver.find_element(By.XPATH, '//button[text()="Browse files"]')
     btn.click()
-    pyautogui.write(os.getcwd()+"/big_horse.jpeg", interval=0.25)
+    pyautogui.write(os.getcwd() + "/big_horse.jpeg", interval=0.25)
     pyautogui.press('return')
     sleep(5)
     driver.close()
+    display.stop()
 
 
 def test_click_tensor_checkbox():
