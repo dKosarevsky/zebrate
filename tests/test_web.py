6f58b934e35aa68a516c941c6685a703a60762ff
if <if>
2020-11-29 15:40:11 +0300

    selenium tests off

--- tests/test_web.py
+++ tests/test_web.py
@@ -9,34 +9,34 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 
 zebrate_url = "https://zebrate.herokuapp.com/"
 # zebrate_url = "http://localhost:8501/"
 horse_url = "https://media.istockphoto.com/photos/wild-horses-running-free-picture-id1019461046?k=6&m=1019461046&s=612x612&w=0&h=RN04ILkViwine-g3B9BjtUmGV_mucSiQgv8mKM-F644="
 
 
-def test_put_image_link():
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-    driver.get(zebrate_url)
-    sleep(10)
-    link_inputer = driver.find_element(By.TAG_NAME, 'input')
-    link_inputer.send_keys(horse_url)  # заполнить
-    link_inputer.send_keys(Keys.RETURN)  # отправить
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    sleep(10)
-    driver.close()
+# def test_put_image_link():
+#     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+#     driver.get(zebrate_url)
+#     sleep(10)
+#     link_inputer = driver.find_element(By.TAG_NAME, 'input')
+#     link_inputer.send_keys(horse_url)  # заполнить
+#     link_inputer.send_keys(Keys.RETURN)  # отправить
+#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+#     sleep(10)
+#     driver.close()
 
 
-def test_send_image_file():
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-    driver.set_window_size(1600, 900)
-    driver.get(zebrate_url)
-    sleep(10)
-    upload_file = driver.find_element(By.XPATH, '//button[text()="Browse files"]')
-    upload_file.send_keys(os.getcwd() + "/big_horse.jpeg")
-    sleep(10)
-    driver.close()
+# def test_send_image_file():
+#     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+#     driver.set_window_size(1600, 900)
+#     driver.get(zebrate_url)
+#     sleep(10)
+#     upload_file = driver.find_element(By.XPATH, '//button[text()="Browse files"]')
+#     upload_file.send_keys(os.getcwd() + "/big_horse.jpeg")
+#     sleep(10)
+#     driver.close()
 
 
 # def test_download_tensor():
@@ -52,14 +52,14 @@ def test_send_image_file():
 #     driver.close()
 
 
-def test_download_horse():
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-    driver.get(zebrate_url)
-    sleep(10)
-    download_horse_image = driver.find_element(By.XPATH, '//*[text()="Download horse"]')
-    download_horse_image.click()
-    sleep(10)
-    driver.close()
+# def test_download_horse():
+#     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+#     driver.get(zebrate_url)
+#     sleep(10)
+#     download_horse_image = driver.find_element(By.XPATH, '//*[text()="Download horse"]')
+#     download_horse_image.click()
+#     sleep(10)
+#     driver.close()
 
 
 # def test_download_zebra():
